da9339faf5bffac9c5fe8ad8e2cdf3d35a32f40f
Tobias <tobias_maier99@gmx.de>
2022-01-03 23:32:18 +0100

    day 14

---
+++ solve14.py
@@ -0,0 +1,180 @@
+import math
+from collections import defaultdict
+from aocd import submit, get_data
+
+
+def main():
+    day = 14
+    year = 2019
+    data = get_data(day=day, year=year)
+
+    test_data_a = {
+        """157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT""": 13312,
+        """2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF""": 180697,
+        """171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX""": 2210736,
+    }
+    test_data_b = {
+        """157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT""": 82892753,
+        """2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF""": 5586022,
+        """171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX""": 460664,
+    }
+
+    for i, (test, true) in enumerate(test_data_a.items()):
+        result = solve_a(test)
+        assert result == true, f"{result} != {true}"
+
+    result_a = solve_a(data)
+    submit(result_a, part="a", day=day, year=year)
+
+    for i, (test, true) in enumerate(test_data_b.items()):
+        result = solve_b(test)
+        assert result == true, f"{result} != {true}"
+
+    result_b = solve_b(data)
+    submit(result_b, part="b", day=day, year=year)
+
+
+class Reactor:
+    store = None
+    chems = None
+
+    def __init__(self, chems):
+        self.chems = chems
+        self.store = defaultdict(int)
+
+    def react(self, chemical, amount):
+        if chemical == "ORE":
+            self.store[chemical] -= amount
+        else:
+            if amount > self.store[chemical]:
+                prodamount = amount - self.store[chemical]
+                ins, out = self.chems[chemical]
+                times = math.ceil(prodamount/out)
+                self.store[chemical] += times * out
+                for c in ins:
+                    self.react(c[1], times * c[0])
+            self.store[chemical] -= amount
+
+    def needed(self):
+        return self.store["ORE"] * -1
+
+
+def solve_a(data):
+    chems = {}
+    for line in data.splitlines():
+        ins, out = line.split("=>")
+        out = out.strip().split(" ")
+        out[0] = int(out[0])
+
+        ix = ins.split(",")
+        for i, _ in enumerate(ix):
+            ix[i] = ix[i].strip().split(" ")
+            ix[i][0] = int(ix[i][0])
+        chems[out[1]] = (ix, out[0])
+    reactor = Reactor(chems)
+    reactor.react("FUEL", 1)
+    amount = -1 * reactor.store["ORE"]
+    return amount
+
+
+def solve_b(data):
+    chems = {}
+    for line in data.splitlines():
+        ins, out = line.split("=>")
+        out = out.strip().split(" ")
+        out[0] = int(out[0])
+
+        ix = ins.split(",")
+        for i, _ in enumerate(ix):
+            ix[i] = ix[i].strip().split(" ")
+            ix[i][0] = int(ix[i][0])
+        chems[out[1]] = (ix, out[0])
+
+    ore = 1000000000000
+    min_ = 0
+    max_ = ore
+
+    while max_ - min_ > 1:
+        times = (max_ - min_) // 2 + min_
+        reactor = Reactor(chems)
+        reactor.react("FUEL", times)
+        if reactor.needed() > ore:
+            max_ = times
+        else:
+            min_ = times
+            max_ += times
+    return min_
+
+
+if __name__ == "__main__":
+    main()
